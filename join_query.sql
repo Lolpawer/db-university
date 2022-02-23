c1e12655082a83a346856c11065e1c2e39649e31
Lolpawer <dadu.petrosino@gmail.com>
2022-02-23 19:00:37 +0100

    ex 4

--- join_query.sql
+++ join_query.sql
@@ -26,7 +26,13 @@ WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
-
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `students`.`degree_id`, `degrees`.`name`, `departments`.`name`
+FROM `students`
+JOIN `degrees`
+	ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+	ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
