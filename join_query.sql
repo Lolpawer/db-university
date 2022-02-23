f5905e9037583fee5ff824c8e872b0cdf6d38231
Lolpawer <dadu.petrosino@gmail.com>
2022-02-23 18:03:06 +0100

    ex 1

--- join_query.sql
+++ join_query.sql
@@ -1,6 +1,10 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
-
+SELECT *
+FROM `students`
+JOIN `degrees`
+	ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
